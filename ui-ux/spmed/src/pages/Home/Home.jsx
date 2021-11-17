af503429d4048a7808fa6b8da44ce2028676fa02
Cardoso-Diniz <guicardosodiniz123@gmail.com>
2021-11-17 10:57:08 -0300

    React adicionado

---
+++ ui-ux/spmed/src/pages/Home/Home.jsx
@@ -0,0 +1,36 @@
+import '../../assets/css/home.css'
+import '../../assets/css/footer.css'
+import Header from '../../components/header/header';
+
+import { Component } from "react";
+
+export default class Home extends Component {
+
+  render() {
+    return (
+      <div>
+        <Header Login="Login" Props={this.props} />
+
+        <main className="main_home">
+          <div className="cor_Home">
+            <div className="container">
+              <h1 className="h1_home">O cuidado completo com a sua saúde<span id="ponto_blue">.</span></h1>
+              <p className="texto_sobre_nos">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+              </p>
+            </div>
+          </div>
+        </main>
+
+        <footer>
+          <div className="container container_footer">
+            <span className="span_footer">
+            Clínica MedGroup - Todos os direitos reservados 2021.
+            </span>
+          </div>
+        </footer>
+      </div>
+
+    )
+
+  }
+}
